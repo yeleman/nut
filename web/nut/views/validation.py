770bbd142b840ef0f38b05234c114b6aa146e756
ksamuel <info.ksamuel@gmail.com>
2012-01-22 00:14:45 +0000

    Validation report form is has all fields and save correctly

--- web/nut/views/validation.py
+++ web/nut/views/validation.py
@@ -8,9 +8,11 @@ import datetime
 from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
+from django.utils.datastructures import SortedDict
+
 
 from bolibana.web.decorators import provider_permission
-from bolibana.models import Period
+from bolibana.models import MonthPeriod
 
 from nutrsc import constants
 
@@ -46,63 +48,90 @@ def report_validation(request, entity, slug, year, month):
 
     data = request.POST or None
 
-    period = Period.find_create_from(year, month)
+    period = MonthPeriod.find_create_from(year=year, month=month)
 
     ctx = {'year': year, 'month': month, 'entity': entity}
 
-    pec_samp_report = extract(entity.nut_pecsampreport_reports.filter(period=period),
-                          0, default=None)
+    qs1 = ConsumptionReport.objects.filter(period=period, entity=entity)
+    qs2 = entity.nut_orderreport_reports.filter(period=period, entity=entity)
 
-    pec_sam_report = extract(entity.nut_pecsamreport_reports.filter(period=period),
-                         0, default=None)
+    reports = SortedDict((
 
-    pec_mam_report = extract(entity.nut_pecmamreport_reports.filter(period=period),
-                         0, default=None)
+    ('pec_samp_report', extract(entity.nut_pecsampreport_reports.filter(period=period),
+                               0, default=None)),
+    ('pec_sam_report', extract(entity.nut_pecsamreport_reports.filter(period=period),
+                               0, default=None)),
+    ('pec_mam_report', extract(entity.nut_pecmamreport_reports.filter(period=period),
+                               0, default=None)),
+    ('pec_other_report', extract(entity.nut_pecothersreport_reports.filter(period=period),
+                                 0, default=None)),
 
-    pec_other_report = extract(entity.nut_pecothersreport_reports.filter(period=period),
-                           0, default=None)
+    ('cons_samp_report', extract(qs1.filter(nut_type=constants.SEVERE_COMP),
+                                 0, default=None)),
+    ('cons_sam_report', extract(qs1.filter(nut_type=constants.SEVERE),
+                                0, default=None)),
+    ('cons_mam_report', extract(qs1.filter(nut_type=constants.MODERATE),
+                                0, default=None)),
 
-    cons_samp_report = extract(ConsumptionReport.objects.filter(period=period,
-                                                                entity=entity,
-                                                                nut_type=constants.SEVERE_COMP),
-                              0, default=None)
+    ('order_samp_report', extract(qs2.filter(nut_type=constants.SEVERE_COMP),
+                                  0, default=None)),
+    ('order_sam_report', extract(qs2.filter(nut_type=constants.SEVERE),
+                                 0, default=None)),
+    ('order_mam_report', extract(qs2.filter(nut_type=constants.MODERATE),
+                                 0, default=None)),
+
+    ))
+
+    if not any(reports.itervalues()):
+      raise Http404
 
 
-    cons_sam_report = extract(ConsumptionReport.objects.filter(period=period,
-                                                               entity=entity,
-                                                               nut_type=constants.SEVERE),
-                              0, default=None)
+    forms = SortedDict((
+        ('pec_samp_form', PECSAMPReportForm(data, instance=reports['pec_samp_report'],
+                                            prefix='pec_samp_report')),
+        ('pec_sam_form', PECSAMReportForm(data, instance=reports['pec_sam_report'],
+                                          prefix='pec_sam_report')),
+        ('pec_mam_form', PECMAMReportReportForm(data, instance=reports['pec_mam_report'],
+                                                prefix='pec_mam_report')),
+        ('pec_other_form', PECOthersReportForm(data, instance=reports['pec_other_report'],
+                                               prefix="pec_other_report")),
 
-    cons_mam_report = extract(ConsumptionReport.objects.filter(period=period,
-                                                               entity=entity,
-                                                               nut_type=constants.MODERATE),
-                              0, default=None)
-
-
-    forms = dict(
-        pec_samp_form = PECSAMPReportForm(data, instance=pec_samp_report),
-        pec_sam_form = PECSAMReportForm(data, instance=pec_sam_report),
-        pec_mam_form = PECMAMReportReportForm(data, instance=pec_mam_report),
-        pec_other_form = PECOthersReportForm(data, instance=pec_other_report),
-        cons_samp_form = InputConsumptionReportFormSet(data,
+        ('cons_samp_form', InputConsumptionReportFormSet(data,
                                                        prefix='cons_samp_report',
-                                                       instance=cons_samp_report),
-
-        cons_sam_form = InputConsumptionReportFormSet(data,
+                                                       instance=reports['cons_samp_report'])),
+        ('cons_sam_form', InputConsumptionReportFormSet(data,
                                                        prefix='cons_sam_report',
-                                                       instance=cons_sam_report),
-
-        cons_mam_form = InputConsumptionReportFormSet(data,
+                                                       instance=reports['cons_sam_report'])),
+        ('cons_mam_form', InputConsumptionReportFormSet(data,
                                                        prefix='cons_mam_report',
-                                                       instance=cons_mam_report)
-    )
+                                                       instance=reports['cons_mam_report'])),
 
-    if all(form.is_valid() for form in forms.itervalues()):
-        for form in forms.itervalues():
+        ('order_samp_form', InputOrderReportFormSet(data,
+                                                  prefix='order_samp_report',
+                                                  instance=reports['order_samp_report'])),
+        ('order_sam_form', InputOrderReportFormSet(data,
+                                                  prefix='order_sam_report',
+                                                  instance=reports['order_sam_report'])),
+        ('order_mam_form', InputOrderReportFormSet(data,
+                                                  prefix='order_mam_report',
+                                                  instance=reports['order_mam_report']))
+    ))
+
+    forms_with_instances = []
+    for i, form in enumerate(forms.itervalues()):
+        if reports.value_for_index(i):
+            forms_with_instances.append(form)
+
+    is_valid = all(form.is_valid() for form in forms_with_instances)
+    if is_valid:
+        for form in forms_with_instances:
             form.save()
-
+        messages.success(request, u'Le rapport de %s pour la '
+                                  u'période du %s/%s a été validé '
+                                  u'avec succès' % (entity, month, year))
 
     ctx = locals()
     ctx.update(forms)
+    ctx.update(reports)
 
     return render(request, 'validation.html', ctx)
