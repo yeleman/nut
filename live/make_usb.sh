7e82015753c8276f847f75cd8e46cc2939090747
renaud gaudin <rgaudin@gmail.com>
2012-02-08 18:43:15 +0000

    only compile 686 flavour

--- live/make_usb.sh
+++ live/make_usb.sh
@@ -1,47 +1,60 @@
 #!/bin/bash
 
 DEVICE=/dev/sdb
+SRC="../nut/live"
 
-if ["$1" -neq ""] then
+if [ "$1" != "" ]; then
     DEVICE=$1
 fi
 
+if [ "$2" != "" ]; then
+    SRC=$2
+fi
+
 echo "Unmounting device $DEVICE"
-umount $DEVICE
+umount ${DEVICE}
 
 sleep 1
 
 echo "Create empty partition table"
-parted -s /dev/sdb mklabel msdos
+parted -s ${DEVICE} mklabel msdos || exit 1
 
-parted -s /dev/sdb mkpart primary fat32 1 4096
-mkfs.vfat -n DONNEES /dev/sdb1
+echo "Create FAT32 partition for data"
+parted -s ${DEVICE} mkpart primary fat32 1 4096 || exit 1
+mkfs.vfat -n DONNEES ${DEVICE}1 || exit 1
 
-parted -s /dev/sdb mkpart primary ext2 4096 6144
-mkfs.ext2 -L DEBIAN_LIVE /dev/sdb2
-tune2fs -c 0 /dev/sdb2
-parted /dev/sdb set 2 boot on
+echo "Create ext2 partition for system"
+parted -s ${DEVICE} mkpart primary ext2 4096 6144 || exit 1
+mkfs.ext2 -L DEBIAN_LIVE ${DEVICE}2 || exit 1
+tune2fs -c 0 ${DEVICE}2 || exit 1
+parted ${DEVICE} set 2 boot on || exit 1
 
-parted -s /dev/sdb mkpart primary ext2 6144 8192
-mkfs.ext4 -L home-rw /dev/sdb3
-tune2fs -c 0 /dev/sdb3
+echo "Create ext4 partition for home"
+parted -s ${DEVICE} mkpart primary ext2 6144 8192 || exit 1
+mkfs.ext4 -L home-rw ${DEVICE}3 || exit 1
+tune2fs -c 0 ${DEVICE}3 || exit 1
 
-echo "Mounting image file"
-#dd if=binary.img of=$DEVICE bs=5M
+echo "Mount image file"
 umount /mnt/img
-mkdir -p /mnt/img
-mount -t vfat -o loop,offset=512 binary.img /mnt/img
+mkdir -p /mnt/img || exit 1
+mount -t vfat -o loop,offset=512 binary.img /mnt/img || exit 1
 sleep 1
-umount /media/DEBIAN_LIVE
-mkdir -p /media/DEBIAN_LIVE
-mount /dev/sdb2 /media/DEBIAN_LIVE/
-cp -r /mnt/img/* /media/DEBIAN_LIVE/
 
-cp ../splash.png ../syslinux.cfg /media/DEBIAN_LIVE/syslinux/
-extlinux --install /media/DEBIAN_LIVE/syslinux
+echo "Mount USB system partition"
 umount /media/DEBIAN_LIVE
-dd bs=440 conv=notrunc count=1 if=/usr/lib/syslinux/mbr.bin of=/dev/sdb
+mkdir -p /media/DEBIAN_LIVE || exit 1
+mount ${DEVICE}2 /media/DEBIAN_LIVE/ || exit 1
+
+echo "Copy image files to USB"
+cp -r /mnt/img/* /media/DEBIAN_LIVE/ || exit 1
+
+echo "Edit syslinux configuration"
+cp $SRC/splash.png $SRC/syslinux.cfg /media/DEBIAN_LIVE/syslinux/ || exit 1
+extlinux --install /media/DEBIAN_LIVE/syslinux || exit 1
+umount /media/DEBIAN_LIVE
+
+echo "Install MBR to USB"
+dd bs=440 conv=notrunc count=1 if=/usr/lib/syslinux/mbr.bin of=${DEVICE} || exit 1
 
 echo "Current partition table"
-parted $DEVICE print
-
+parted ${DEVICE} print || exit 1
