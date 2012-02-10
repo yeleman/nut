c302b6fb47340a0d5e4805dbfd076ee18645dd89
renaud gaudin <rgaudin@gmail.com>
2012-02-10 10:02:51 +0000

    added XLS exports path to config

--- live/local_config.py
+++ live/local_config.py
@@ -2,4 +2,8 @@
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4 nu
 
+# path to client DB containing reports, etc
 SQLITE_PATH = '/home/nut/nutrition.db'
+
+# path to exports repository
+EXPORT_PATH = '/media/export/export_nutrition'
