5604eb472d646e87c7ff91db48a80ce7e3d2bead
renaud gaudin <rgaudin@gmail.com>
2012-02-16 11:45:42 +0000

    added web dashboard

--- web/nut/views/dashboard.py
+++ web/nut/views/dashboard.py
@@ -101,6 +101,85 @@ def dashboard(request):
     category = 'dashboard'
     context = {'category': category}
 
+    from bolibana.models import Entity
+    from nut.models import NutritionReport
+    from nut.data import (current_period, \
+                          current_reporting_period, current_stage, \
+                          time_cscom_over, time_district_over, \
+                          time_region_over, contact_for)
+
+    def sms_received_sent_by_period(period):
+        from nosmsd.models import Inbox, SentItems
+        received = Inbox.objects\
+                        .filter(receivingdatetime__gte=period.start_on,
+                                receivingdatetime__lte=period.end_on).count()
+        sent = SentItems.objects\
+                        .filter(sendingdatetime__gte=period.start_on,
+                                sendingdatetime__lte=period.end_on).count()
+        return (received, sent)
+
+    current_period = current_period()
+    period = current_reporting_period()
+
+    context.update({'current_period': current_period,
+                    'current_reporting_period': period,
+                    'current_stage': current_stage(),
+                    'current_sms': sms_received_sent_by_period(current_period),
+                    'current_reporting_sms': \
+                         sms_received_sent_by_period(period),
+                    'total_cscom': Entity.objects\
+                                         .filter(type__slug='cscom').count(),
+                    'time_cscom_over': time_cscom_over(period),
+                    'time_district_over': time_district_over(period),
+                    'time_region_over': time_region_over(period)})
+
+    received_cscom_reports = \
+        NutritionReport.objects.filter(period=period, entity__type__slug='cscom')
+    cscom_reports_validated = \
+        NutritionReport.validated.filter(period=period, \
+                                       entity__type__slug='cscom')
+    district_reports_validated = \
+        NutritionReport.validated.filter(period=period, \
+                                       entity__type__slug='district')
+    reporting_rate = \
+        float(NutritionReport.validated.filter(period=period).count()) \
+        / Entity.objects.count()
+
+    cscom_missed_report = \
+        Entity.objects.filter(type__slug='cscom')\
+                      .exclude(id__in=[r.entity.id \
+                                       for r \
+                                       in received_cscom_reports])\
+                      .order_by('name')
+
+    def entities_autoreports(level):
+        districts_missed_report = {}
+        auto_validated_cscom_reports = \
+            NutritionReport.validated\
+                         .filter(entity__type__slug=level, \
+                                 modified_by__user__username='autobot')
+        for report in auto_validated_cscom_reports:
+            if not report.entity.parent.slug in districts_missed_report:
+                districts_missed_report[report.entity.parent.slug] = \
+                    {'entity': report.entity.parent, \
+                     'nbauto': 0, \
+                     'contact': contact_for(report.entity.parent, False)}
+            districts_missed_report[report.entity.parent.slug]['nbauto'] += 1
+        return districts_missed_report
+
+    districts_missed_report = entities_autoreports('cscom')
+    regions_missed_report = entities_autoreports('district')
+
+    context.update({'received_cscom_reports': received_cscom_reports.count(),
+                'cscom_reports_validated': cscom_reports_validated.count(),
+              'district_reports_validated': district_reports_validated.count(),
+                'reporting_rate': reporting_rate,
+                'cscom_missed_report_count': cscom_missed_report.count(),
+                'cscom_missed_report': [(e, contact_for(e, True)) \
+                                        for e in cscom_missed_report[:20]],
+                'districts_missed_report': districts_missed_report,
+                'regions_missed_report': regions_missed_report})
+
     return render(request, 'dashboard.html', context)
 
 
