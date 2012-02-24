cfdb09a16ec91a2e09da968efe1d30684dade8a7
Fadiga <ibfadiga@gmail.com>
2012-02-24 15:20:29 +0000

    Fatorisation et application du pep8

--- nutrsc/export.py
+++ nutrsc/export.py
@@ -29,29 +29,29 @@ borders.right = 1
 borders.top = 1
 borders.bottom = 1
 
-al = xlwt.Alignment()
-al.horz = xlwt.Alignment.HORZ_CENTER
-al.vert = xlwt.Alignment.VERT_CENTER
+center_align = xlwt.Alignment()
+center_align.horz = xlwt.Alignment.HORZ_CENTER
+center_align.vert = xlwt.Alignment.VERT_CENTER
 
-left_al = xlwt.Alignment()
-left_al.vert = xlwt.Alignment.HORZ_LEFT
+left_align = xlwt.Alignment()
+left_align.vert = xlwt.Alignment.HORZ_LEFT
 
-pat2 = xlwt.Pattern()
-pat2.pattern = xlwt.Pattern.SOLID_PATTERN
-pat2.pattern_fore_colour = 23
+grey44_color = xlwt.Pattern()
+grey44_color.pattern = xlwt.Pattern.SOLID_PATTERN
+grey44_color.pattern_fore_colour = 23
 
 style_title = xlwt.XFStyle()
 style_title.font = font
-style_title.alignment = al
+style_title.alignment = center_align
 
 styleheader = xlwt.XFStyle()
 styleheader.borders = borders
-styleheader.alignment = al
+styleheader.alignment = center_align
 styleheader.font = font_gras
 
 styleheader_ = xlwt.XFStyle()
 styleheader_.borders = borders
-styleheader_.alignment = al
+styleheader_.alignment = center_align
 
 styleheader_left = xlwt.XFStyle()
 styleheader_left.borders = borders
@@ -60,16 +60,16 @@ style_without_border = xlwt.XFStyle()
 
 style_value = xlwt.XFStyle()
 style_value.borders = borders
-style_value.alignment = al
+style_value.alignment = center_align
 
 styleblack = xlwt.XFStyle()
-styleblack.pattern = pat2
+styleblack.pattern = grey44_color
 styleblack.borders = borders
 
 styleheader_left_g = xlwt.XFStyle()
 styleheader_left_g.borders = borders
 styleheader_left_g.font = font_gras
-styleheader_left_g.alignment = left_al
+styleheader_left_g.alignment = left_align
 
 
 def val(report, field):
@@ -103,7 +103,8 @@ def export_report_stream(report):
         others_tb = report.pec_other_report.other_tb
         others_lwb = report.pec_other_report.other_lwb
         # retrieve InputOrderReport.objects.filter without importing
-        ior_class = list(report.order_reports()[0].input_order_reports.all())[0].__class__.objects.filter
+        ior_class = list(report.order_reports()[0]\
+                    .input_order_reports.all())[0].__class__.objects.filter
     else:
         hc_name = report.hc_name
         issamp = report.hc_issamp
@@ -115,7 +116,7 @@ def export_report_stream(report):
         ior_class = InputOrderReport.filter
 
     def write_merge_p(liste, style):
-        
+
         for index in liste:
             label = index.keys()[0]
             row, row1, col, col1 = index.values()[0]
@@ -137,7 +138,7 @@ def export_report_stream(report):
             on_row += i_row
 
     def create_values(hc, liste, prefix):
-        liste_val = [val(hc, "%s%s" % (prefix, i)) for i in liste]
+        liste_val = [val(hc, "%s%s" % (prefix, v)) for v in liste]
         return liste_val
 
     def type_center():
@@ -178,20 +179,20 @@ def export_report_stream(report):
     write_p(type_centers, 27, 4, 0, 0, styleheader_left_g)
 
     #Tableau1 header
-    header1 = [{u"Groupe \nd'age": [6, 8, 0, 0]},
-               {u"Total au \ndébut \nmois(A1)": [6, 8, 1, 1]},
+    header1 = [{u"Groupe\nd'age": [6, 8, 0, 0]},
+               {u"Total au\ndébut\nmois(A1)": [6, 8, 1, 1]},
                {u"Sexe(A2)": [6, 7, 2, 3]},
                {u"Admissions(B)": [6, 6, 4, 17]},
                {u"Critere d'admissions (B1)": [7, 7, 4, 9]},
                {u"Autre": [8, 8, 9, 9]},
                {u"Type d'admission (B2)": [7, 7, 10, 14]},
-               {u"Total \nAdmissions \n (B3)": [7, 8, 15, 15]},
-               {u"Sexe (B4)": [7, 7, 16, 17]},]
+               {u"Total\nAdmissions\n(B3)": [7, 8, 15, 15]},
+               {u"Sexe (B4)": [7, 7, 16, 17]}]
     write_merge_p(header1, styleheader)
 
     header1_ = [{u"M": [8, 8, 2, 2]},
                {u"F": [8, 8, 3, 3]},
-               {u"P/T \n≥70< 80% \nou\nIMC<18": [8, 8, 4, 4]},
+               {u"P/T\n≥70< 80%\nou\nIMC<18": [8, 8, 4, 4]},
                {u"PB <120\nmm (ou PB\n<210 mm)": [8, 8, 5, 5]},
                {u"P/T <70%\nou IMC\n<16 ": [8, 8, 6, 6]},
                {u"PB <11 cm\n(ou PB\n<18cm) ": [8, 8, 7, 7]},
@@ -202,7 +203,7 @@ def export_report_stream(report):
                {u"Transfert\nnutritionnel": [8, 8, 13, 13]},
                {u"Référence\nnutritionnelle": [8, 8, 14, 14]},
                {u"M": [8, 8, 16, 16]},
-               {u"F": [8, 8, 17, 17]},]
+               {u"F": [8, 8, 17, 17]}]
     write_merge_p(header1_, styleheader_)
 
     type_ureni_1 = ["< 6 mois", "6-59 mois", ">59 mois"]
@@ -217,49 +218,48 @@ def export_report_stream(report):
     sheet.write(20, 0, u"Suivi de 1 et 2", styleheader_left)
     sheet.write(38, 0, u"Suivi de 3 et 2", styleheader_left)
     l_val = ['_total_beginning', '_total_beginning_m', '_total_beginning_f',
-        '_hw_b7080_bmi_u18', '_muac_u120', '_hw_u70_bmi_u16',
-        '_muac_u11_muac_u18', '_oedema', '_other', '_new_case', '_relapse',
-        '_returned', '_nut_transfered_in', '_nut_referred_in', '_admitted',
-        '_admitted_m', '_admitted_f']
+             '_hw_b7080_bmi_u18', '_muac_u120', '_hw_u70_bmi_u16',
+             '_muac_u11_muac_u18', '_oedema', '_other', '_new_case',
+             '_relapse', '_returned', '_nut_transfered_in', '_nut_referred_in',
+             '_admitted', '_admitted_m', '_admitted_f']
     # Valeurs tableau1
     # URENI 1
     write_p(empty(16), 9, 0, 1, 1)
     # < 6 mois
-    u6_pec_samp_reports = create_values(report.pec_samp_report, l_val, 'u6')
-    write_p(u6_pec_samp_reports, 10, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_samp_report, l_val, 'u6'),
+                                                     10, 0, 1, 1, style_value)
     # 6-59 mois
-    u59_pec_samp_reports = create_values(report.pec_samp_report, l_val, 'u59')
-    write_p(u59_pec_samp_reports, 11, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_samp_report, l_val, 'u59'),
+                                                     11, 0, 1, 1, style_value)
     # > 59 mois
-    o59_pec_samp_reports = create_values(report.pec_samp_report, l_val, 'o59')
-    write_p(o59_pec_samp_reports, 12, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_samp_report, l_val, 'o59'),
+                                                     12, 0, 1, 1, style_value)
 
     # URENAS 2
     write_p(empty(16), 13, 0, 1, 1)
     # 6-59 mois
-    u59_pec_sam_reports = create_values(report.pec_sam_report, l_val, 'u59')
-    write_p(u59_pec_sam_reports, 14, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_sam_report, l_val, 'u59'),
+                                                     14, 0, 1, 1, style_value)
     # > 59 mois
-    o59_pec_sam_reports = create_values(report.pec_sam_report, l_val, 'o59')
-    write_p(o59_pec_sam_reports, 15, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_sam_report, l_val, 'o59'),
+                                                     15, 0, 1, 1, style_value)
     # SUIVI DE URENI 1
-    fu1_pec_sam_reports = create_values(report.pec_sam_report, l_val, 'fu1')
-    write_p(fu1_pec_sam_reports, 16, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_sam_report, l_val, 'fu1'),
+                                                     16, 0, 1, 1, style_value)
 
     # URENAM 3
     write_p(empty(16), 17, 0, 1, 1)
     # 6-59 mois
-    u59_pec_mam_reports = create_values(report.pec_mam_report, l_val, 'u59')
-    write_p(u59_pec_mam_reports, 18, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_mam_report, l_val, 'u59'),
+                                                     18, 0, 1, 1, style_value)
     # > FE/FA
-    pw_pec_mam_reports = create_values(report.pec_mam_report, l_val, 'pw')
-    write_p(pw_pec_mam_reports, 19, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_mam_report, l_val, 'pw'),
+                                                     19, 0, 1, 1, style_value)
     # SUIVI DE 1 ET 2
-    fu12_pec_mam_reports = create_values(report.pec_mam_report, l_val, 'fu12')
-    write_p(fu12_pec_mam_reports, 20, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_mam_report, l_val, 'fu12'),
+                                                     20, 0, 1, 1, style_value)
     # TOTAL
-    sum_all_reports = create_values(report, l_val, 'sum_all')
-    write_p(sum_all_reports, 21, 0, 1, 1, style_value)
+    write_p(create_values(report, l_val, 'sum_all'), 21, 0, 1, 1, style_value)
 
     # Tableau 2
     # header
@@ -272,6 +272,7 @@ def export_report_stream(report):
                {u"Total\n (D1)": [24, 26, 13, 13]},
                {u"Sexe (D2)": [24, 24, 14, 15]}]
     write_merge_p(header2, styleheader)
+
     header2_ = [{u"Gueris": [25, 26, 1, 3]},
                 {u"Referes\nvers 3 ou\n2": [25, 26, 4, 4]},
                 {u"Deces": [25, 26, 5, 5]},
@@ -284,46 +285,46 @@ def export_report_stream(report):
                 {u"M": [25, 26, 14, 14]},
                 {u"F": [25, 26, 15, 15]}]
     write_merge_p(header2_, styleheader_)
-    l_val1 = ['_healed', '_referred_out', '_deceased',
-          '_aborted', '_non_respondant', '_medic_transfered_out',
-          '_nut_transfered_out', '_total_out', '_total_out_m',
-          '_total_out_f', '_total_end', '_total_end_m',
-          '_total_end_f']
+
+    l_val1 = ['_healed', '_referred_out', '_deceased', '_aborted',
+              '_non_respondant', '_medic_transfered_out',
+              '_nut_transfered_out', '_total_out', '_total_out_m',
+              '_total_out_f', '_total_end', '_total_end_m', '_total_end_f']
     # URENI 1
     write_p(empty(12), 27, 0, 1, 1)
     # < 6 mois
-    u6_pec_samp_reports_out = create_values(report.pec_samp_report, l_val1, 'u6')
-    write_p(u6_pec_samp_reports_out, 28, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_samp_report, l_val1, 'u6'),
+                                                     28, 0, 1, 1, style_value)
     # < 6-59  mois
-    u59_pec_samp_reports_out = create_values(report.pec_samp_report, l_val1, 'u59')
-    write_p(u59_pec_samp_reports_out, 29, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_samp_report, l_val1, 'u59'),
+                                                     29, 0, 1, 1, style_value)
     # > 59  mois
-    o59_pec_samp_reports_out = create_values(report.pec_samp_report, l_val1, 'o59')
-    write_p(o59_pec_samp_reports_out, 30, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_samp_report, l_val1, 'o59'),
+                                                     30, 0, 1, 1, style_value)
 
     # URENAS 2
     write_p(empty(12), 31, 0, 1, 1)
     # < 6-59  mois
-    u59_pec_sam_reports_out = create_values(report.pec_sam_report, l_val1, 'u59')
-    write_p(u59_pec_sam_reports_out, 32, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_sam_report, l_val1, 'u59'),
+                                                     32, 0, 1, 1, style_value)
     # > 59 mois
-    o59_pec_sam_reports_out = create_values(report.pec_sam_report, l_val1, 'o59')
-    write_p(o59_pec_sam_reports_out, 33, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_sam_report, l_val1, 'o59'),
+                                                     33, 0, 1, 1, style_value)
     # SUIVI DE URENI 1
-    fu1_pec_sam_reports_out = create_values(report.pec_sam_report, l_val1, 'fu1')
-    write_p(fu1_pec_sam_reports_out, 34, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_sam_report, l_val1, 'fu1'),
+                                                     34, 0, 1, 1, style_value)
 
     # URENAM 3
     write_p(empty(12), 35, 0, 1, 1)
     # 6-59 mois
-    u59_pec_mam_reports_out = create_values(report.pec_mam_report, l_val1, 'u59')
-    write_p(u59_pec_mam_reports_out, 36, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_mam_report, l_val1, 'u59'),
+                                                     36, 0, 1, 1, style_value)
     # > FE/FA
-    pw_pec_mam_reports_out = create_values(report.pec_mam_report, l_val1, 'pw')
-    write_p(pw_pec_mam_reports_out, 37, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_mam_report, l_val1, 'pw'),
+                                                     37, 0, 1, 1, style_value)
     # SUIVI DE 3 ET 2
-    fu12_pec_mam_reports_out = create_values(report.pec_mam_report, l_val1, 'fu12')
-    write_p(fu12_pec_mam_reports_out, 38, 0, 1, 1, style_value)
+    write_p(create_values(report.pec_mam_report, l_val1, 'fu12'),
+                                                     38, 0, 1, 1, style_value)
     # TOTAL
     sum_all_reports_out = create_values(report, l_val1, 'sum_all')
     write_p(sum_all_reports_out, 39, 0, 1, 1, style_value)
@@ -333,7 +334,7 @@ def export_report_stream(report):
                {u"Spécifier autres admissions": [41, 41, 0, 2]},
                {u"Nombre de PV VIH": [42, 42, 0, 1]},
                {u"Nombre de Tuberculeux": [42, 42, 4, 6]},
-               {u"PPN": [42, 42, 9, 9]},]
+               {u"PPN": [42, 42, 9, 9]}]
     write_merge_p(footers, style_without_border)
 
     footers_values = [{u"%s" % others_hiv: [42, 42, 2, 2]},
@@ -341,7 +342,7 @@ def export_report_stream(report):
                       {u"%s" % others_lwb: [42, 42, 10, 10]}]
     write_merge_p(footers_values, style_value)
 
-    #--------------------------------------------------------------------------#
+    #-------------------------------------------------------------------------#
     # INTRANTS
     sheet = book.add_sheet("INTRANTS")
     for i in range(7):
@@ -353,19 +354,22 @@ def export_report_stream(report):
                       u"RAPPORT MENSUEL SUR LA SITUATION DES INTRANTS PEC MA ",
                        style_title)
 
-    liste_title = [u"REGION", u"District", u"CSCom", u"TYPE DE CENTRE", u"Mois"]
+    liste_title = [u"REGION", u"District", u"CSCom",
+                   u"TYPE DE CENTRE", u"Mois"]
     write_p(liste_title, 2, 1, 0, 0, styleheader_left)
     val_title = [u"Gao", u" ", u"%s" % hc_name, u"%s" % type_center(),
                  u"%s" % report.period]
     write_p(val_title, 2, 1, 1, 0, styleheader_left)
-    sheet.write(7,0, u"Les champs ci-dessus sont à remplir dans la feuille PEC.")
+    sheet.write(7, 0, u"Les champs ci-dessus sont à remplir dans "
+                      u"la feuille PEC.")
 
     #Tableau des intrants
     h_table_intrant = [u"Désignation", u"Stock Initial",
                        u"Quantités Reçues\npendant la période",
                        u"Quantités Utlisées\npendant la période",
                        u"Quantités Perdues*\npendant la période",
-                       u"Quantités Disponibles\nUtilisables en Fin de\npériode",
+                       u"Quantités Disponibles\nUtilisables en "
+                       u"Fin de\npériode",
                        u"Quantités commandées"]
     write_p(h_table_intrant, 9, 0, 0, 1, styleheader)
 
@@ -419,9 +423,9 @@ def export_report_stream(report):
     sheet.write(27, 0, u"Stock début du mois = Solde fin du mois précédent "
                        u"(Ex: Stock début du mois de juin = solde fin de "
                        u"mois de mai)", styleheader_left_g)
-    sheet.write(28, 0, u"Solde fin du mois = [Stock debut du mois + Stock recu "
-                       u"(facultatif)] - [Stock utilisé + Stock perdu/avarié "
-                       u"(facultatif)]", styleheader_left_g)
+    sheet.write(28, 0, u"Solde fin du mois = [Stock debut du mois + Stock "
+                       u"recu (facultatif)] - [Stock utilisé + Stock perdu/"
+                       u"avarié (facultatif)]", styleheader_left_g)
 
     sheet.write_merge(30, 30, 0, 6, "OBSERVATIONS", styleheader)
     sheet.write_merge(31, 44, 0, 6, "", style_value)
@@ -437,7 +441,7 @@ def export_report(report, filepath=None):
     if not filepath:
         filepath = 'report-%s.xls' % str(report.period)
     print('exporting %s to %s' % (report, filepath))
- 
+
     stream = export_report_stream(report)
 
     f = file(filepath, 'w')
